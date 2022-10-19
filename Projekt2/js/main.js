314987252eb8b6c01f075a0df18a28bc72b2e240
matd127 <mateusz333555@gmail.com>
2022-10-19 23:41:28 +0200

    Automatyczny slider

--- Projekt2/js/main.js
+++ Projekt2/js/main.js
@@ -1,41 +1,70 @@
-const slide = document.querySelector(".slides-slide")
-const slideImages = document.querySelectorAll(".slides-slide img")
+const slide = document.querySelector(".slides-slide");
+const slideImages = document.querySelectorAll(".slides-slide img");
+const slideContainer = document.querySelector(".slides-container");
 
-const prevBtn = document.querySelector(".prev-btn")
-const nextBtn = document.querySelector(".next-btn")
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
-const navigationDots = document.querySelectorAll(".navigation-dots .single-dot")
+const navigationDots = document.querySelectorAll(
+  ".navigation-dots .single-dot"
+);
 
 let counter = 0;
-const size = slideImages[0].clientWidth
+const size = slideImages[0].clientWidth;
 
-nextBtn.addEventListener('click', () => {
-    slide.style.transition = "transform 0.4s ease-in-out"
-    counter++;
-    if(counter > 2){
-        counter = 0
-        slide.style.transition = "transform 0.4s ease-in-out"
-        slide.style.transform = 'translateX(' + (-600 * counter) + 'px)'
-    }
-    slide.style.transform = 'translateX(' + (-600 * counter) + 'px)'
-})
+nextBtn.addEventListener("click", () => {
+  slide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  if (counter > 2) {
+    counter = 0;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = "translateX(" + -600 * counter + "px)";
+  }
+  slide.style.transform = "translateX(" + -600 * counter + "px)";
+});
 
-prevBtn.addEventListener('click', () => {
-    slide.style.transition = "transform 0.4s ease-in-out"
-    counter--;
-    if(counter < 0){
-        counter = 2
-        slide.style.transition = "transform 0.4s ease-in-out"
-        slide.style.transform = 'translateX(' + (-600 * counter) + 'px)'
-    }
-    slide.style.transform = 'translateX(' + (-600 * counter) + 'px)'
-})
+prevBtn.addEventListener("click", () => {
+  slide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  if (counter < 0) {
+    counter = 2;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = "translateX(" + -600 * counter + "px)";
+  }
+  slide.style.transform = "translateX(" + -600 * counter + "px)";
+});
 
-for(let i = 0; i < navigationDots.length; i++){
-    navigationDots[i].addEventListener("click", () => {
-        counter = i;
-        slide.style.transition = "transform 0.4s ease-in-out"
-        slide.style.transform = 'translateX(' + (-600 * counter) + 'px)'
-    })
+for (let i = 0; i < navigationDots.length; i++) {
+  navigationDots[i].addEventListener("click", () => {
+    counter = i;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = "translateX(" + -600 * counter + "px)";
+  });
 }
 
+const moveSlide = () => {
+  counter++;
+  slide.style.transition = "transform 0.4s ease-in-out";
+  slide.style.transform = "translateX(" + -600 * counter + "px)";
+
+  if (counter > 2) {
+    counter = 0;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = "translateX(" + -600 * counter + "px)";
+  }
+};
+
+const intervalRef = setInterval(moveSlide, 1000);
+
+for (let i = 0; i < slideImages.length; i++) {
+  slideImages[i].addEventListener("mouseover", () => {
+    console.log("Stop");
+    clearInterval(intervalRef);
+  });
+}
+for (let i = 0; i < slideImages.length; i++) {
+  slideImages[i].addEventListener("mouseout", () => {
+    console.log("Start");
+    setInterval(moveSlide, 1000);
+  });
+}
