929142c791117502cb5d635294cc4acb6fb80dd9
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-11-01 23:07:20 +0100

    Drumkit na przycisk

--- Projekt3/drumkit.js
+++ Projekt3/drumkit.js
@@ -1,17 +1,29 @@
-const START_RECORDING_KEY = '1'
-document.addEventListener('keypress', onKeyPress)
+// const START_RECORDING_KEY = '1'
+// document.addEventListener('keypress', onKeyPress)
 
-function onKeyPress(event){
-    console.log(event)
-    const key = event.key
-    const audioId = 'clap'
-    playSound(audioId)
-}
+// function onKeyPress(event){
+//     console.log(event)
+//     const key = event.key
+//     const audioId = 'clap'
+//     playSound(audioId)
+// }
 
-function playSound(sound) {
-    const audioTag = document.querySelector(`#${sound}`)
-    audioTag.currentTime = 0;
-    audioTag.play()
-}
+// function playSound(sound) {
+//     const audioTag = document.querySelector(`#${sound}`)
+//     audioTag.currentTime = 0;
+//     audioTag.play()
+// }
 
 //Date.now
+
+document.addEventListener("keydown", (e) => {
+  let audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  let tune = document.querySelector(`.tune[data-key='${e.keyCode}']`);
+  tune.classList.add("playing");
+  audio.currentTime = 0;
+  if (audio != null) audio.play();
+  
+  setTimeout(function () {
+    tune.classList.remove("playing");
+  }, 100);
+});
