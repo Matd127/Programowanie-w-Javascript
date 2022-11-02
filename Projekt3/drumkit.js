c2724b2da1efd552d36135092ba3810bae30ac44
matd127 <mateusz333555@gmail.com>
2022-11-02 23:06:25 +0100

    Nagrywanie, do skończenia

--- Projekt3/drumkit.js
+++ Projekt3/drumkit.js
@@ -15,15 +15,50 @@
 // }
 
 //Date.now
+const RECORDING_KEY = '82';
+const PLAYING_KEY = '80';
+let isPlaying = false;
+let isRecording = false;
+let tunes = [];
+let timing = [];
 
-document.addEventListener("keydown", (e) => {
-  let audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-  let tune = document.querySelector(`.tune[data-key='${e.keyCode}']`);
-  tune.classList.add("playing");
-  audio.currentTime = 0;
-  if (audio != null) audio.play();
-  
-  setTimeout(function () {
-    tune.classList.remove("playing");
-  }, 100);
-});
+
+document.addEventListener("keydown", (event) => {
+  let audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+  let tune = document.querySelector(`.tune[data-key='${event.keyCode}']`);
+  if(tune != null) {
+    tune.classList.add("playing");
+    audio.currentTime = 0;
+    if (audio != null) audio.play();
+    
+    setTimeout(function () {
+      tune.classList.remove("playing");
+    }, 100);
+  }
+  });
+
+function initRecording() {
+  isRecording = true;
+  tunes.splice(0, tunes.length);
+  timing.splice(0, timing.length)
+}
+
+function stopRecording() {
+  isRecording = false;
+}
+
+function recording(keyCode){
+  tunes.push(keyCode);
+  timing.push(Date.now());
+}
+
+function recordingControl(event){
+  if(event.keyCode == RECORDING_KEY){
+      if(!isRecording)
+        initRecording();
+      else
+        stopRecording();
+  }
+}
+
+window.addEventListener("keydown", recordingControl)
