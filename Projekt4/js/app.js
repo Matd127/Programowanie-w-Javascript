1b9addf97557407db35a15ce4132596ed5c9f451
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-11-28 06:39:39 +0100

    Wyświetlanie notatek

--- Projekt4/js/app.js
+++ Projekt4/js/app.js
@@ -1,58 +1,76 @@
-const Note = {
-    id: Number,
-    title: String,
-    content: String,
-    color: String,
-    pin: Boolean,
-    dateOfCreation: Date
+class Note {
+  constructor(id, title, content, color, pin, dateOfCreation, tags) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.color = color;
+    this.pin = pin;
+    this.dateOfCreation = dateOfCreation;
+    this.tags = tags;
+  }
 }
 
-const notes = [];
+class Tag {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
+let notes = [];
 const addNote = document.querySelector("#submit");
 const btnShowForm = document.querySelector(".add-note");
-const form = document.querySelector('.form');
-const overlay = document.querySelector('.overlay');
-const notesList = document.querySelector('.notes-list');
+const form = document.querySelector(".form");
+const overlay = document.querySelector(".overlay");
+const notesList = document.querySelector(".notes-list");
 
-addNote.addEventListener("click", (event) => {
-    event.preventDefault();
-    const title = document.querySelector('input[name="title"]').value
-    const content = document.querySelector('textarea').value
-    const color = document.querySelector('input[name="color"]:checked').id;
-    console.log(color);
-    console.log(Date.now())
+const loadNotes = () => {
+  const jsonNotes = JSON.parse(window.localStorage.getItem("notes"));
+  jsonNotes ? (notes = [...jsonNotes]) : console.log("Brak notatek");
+};
+loadNotes();
 
-    // Note = new Note(Date.now(), title, content, color, false, Date.now())
-    Note.id = Date.now();
-    Note.title = title;
-    Note.content = content;
-    Note.color = color;
-    Note.pin = false;
-    Note.dateOfCreation = Date.now();
-    notes.push(Note);
+const displayNotes = () => {
+  notes &&
+    notes.map((obj) => {
+      const newNote = document.createElement("div");
+      newNote.classList.add("note");
+      newNote.innerHTML =
+        "<div class='delete-button' id=" +
+        obj.id +
+        ">&#10060;</div> <h1> " +
+        obj.title +
+        '</h1> <div class="content">' +
+        obj.content +
+        "</div> ";
+      notesList.appendChild(newNote);
+    });
+};
+displayNotes();
 
-    localStorage.setItem("notes", JSON.stringify(notes))
-})
+addNote.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector('input[name="title"]').value;
+  const content = document.querySelector("textarea").value;
+  const color = document.querySelector('input[name="color"]:checked').id;
+  const note = new Note(Date.now(), title, content, color, false, Date.now());
+  notes.push(note);
+  console.log(notes);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  document.querySelector("form").reset();
+  closeForm();
+  alert("Dodano notatkę!");
+  displayNotes();
+});
 
 const openForm = () => {
-    form.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+  form.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 const closeForm = () => {
-    form.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
-const loadNotes = () => {
-    const jsonData = JSON.parse(window.localStorage.getItem('notes'))
-    console.log(jsonData)
-    const cos = jsonData.map((arr) => {
-        return (arr.title);
-    })
-    console.log(cos.title)
-    notesList.textContent = cos;
+  form.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-}
-loadNotes();
-btnShowForm.addEventListener('click', openForm)
-overlay.addEventListener('click', closeForm)
+btnShowForm.addEventListener("click", openForm);
+overlay.addEventListener("click", closeForm);
--- Projekt4/js/note.js
+++
@@ -1,9 +0,0 @@
-// const Note = {
-//     title: String,
-//     content: String,
-//     color: String,
-//     pin: Boolean,
-//     dateOfCreation: Date
-// }
-
-// export default Note
