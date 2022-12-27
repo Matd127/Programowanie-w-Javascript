ab59079f30d663f1ff3cf6a0636997e7ff34f7d9
Matd127 <79983536+Matd127@users.noreply.github.com>
2022-12-27 19:11:09 +0100

    Wyglad, tagi, naprawa bledow

--- Projekt4/js/app.js
+++ Projekt4/js/app.js
@@ -4,41 +4,53 @@ const loadNotes = () => {
 };
 loadNotes();
 
+function returnDate(date) {
+  const day = `${date.getDate()}`.padStart(2, '0');
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
 const displayNotes = (notes) => {
   notesList.innerHTML = "";
   notes.forEach((note) => {
     const newNote = document.createElement("div");
     newNote.classList.add("note");
     newNote.style.backgroundColor = note.color;
-    newNote.innerHTML = `<div class='manage-icons'> 
-    <div class='edit-button' id=" ${note.id}">📜</div> 
-    <div class='delete-button' id="${note.id}">&#10060;</div> </div>
+    newNote.innerHTML = 
+    `<div class='manage-icons'> 
+      <div class='edit-button' id=" ${note.id}">📜</div> 
+      <div class='delete-button' id="${note.id}">&#10060;</div> 
+    </div>
     <h1>${note.title}</h1> 
-    <div class="content">
-    <div>Tagi: ${note.tags}</div>
+    <div class="note--content">
+      <div class="note--tags">Tagi: ${note.tags} </div>
       ${note.content}
-    </div>`;
+    </div>
+    <div class="note--date">Data utworzenia: ${returnDate(new Date(note.dateOfCreation))} <div>`;
     notesList.appendChild(newNote);
+    console.log(note.tags)
   });
 };
+displayNotes(notes);
+
+addNote.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector('input[name="title"]').value;
+  const content = document.querySelector("textarea").value;
+  const color = document.querySelector('input[type="color"]').value;
+  const tags = (document.querySelector('input[name="tags"]').value).split(' ');
+  const note = new Note(Date.now(), title, content, color, false, tags, Date.now());
+  notes.push(note);
+  console.log(notes)
+   localStorage.setItem("notes", JSON.stringify(notes));
+   document.querySelector("form").reset();
+   toggleForm();
+   displayNotes(notes);
+   manageNotes();
+});
 
 const manageNotes = () => {
-  displayNotes(notes);
-  addNote.addEventListener("click", (e) => {
-    e.preventDefault();
-    const title = document.querySelector('input[name="title"]').value;
-    const content = document.querySelector("textarea").value;
-    const color = document.querySelector('input[type="color"]').value;
-    const tags = (document.querySelector('input[name="tags"]').value).split(' ');
-    const note = new Note(Date.now(), title, content, color, false, tags, Date.now());
-    notes.push(note);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    document.querySelector("form").reset();
-    toggleForm();
-    displayNotes(notes);
-    manageNotes();
-  });
-
   const btnDelete = document.querySelectorAll(".delete-button").forEach(function (btn) {
       btn.addEventListener("click", () => {
         notes = notes.filter((note) => note.id !== Number(btn.id));
