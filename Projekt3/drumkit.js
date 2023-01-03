9270f2ab8c413cac11296c6bf8e80caff172b54a
Matd127 <79983536+Matd127@users.noreply.github.com>
2023-01-03 12:35:16 +0100

    Zapisywanie nut do tablicy

--- Projekt3/drumkit.js
+++ Projekt3/drumkit.js
@@ -1,64 +1,86 @@
-// const START_RECORDING_KEY = '1'
-// document.addEventListener('keypress', onKeyPress)
-
-// function onKeyPress(event){
-//     console.log(event)
-//     const key = event.key
-//     const audioId = 'clap'
-//     playSound(audioId)
-// }
-
-// function playSound(sound) {
-//     const audioTag = document.querySelector(`#${sound}`)
-//     audioTag.currentTime = 0;
-//     audioTag.play()
-// }
-
-//Date.now
-const RECORDING_KEY = '82';
-const PLAYING_KEY = '80';
+document.addEventListener("keypress", onKeyPress);
+const RECORDING_KEY = "82";
+const PLAYING_KEY = "80";
 let isPlaying = false;
 let isRecording = false;
-let tunes = [];
-let timing = [];
 
-
-document.addEventListener("keydown", (event) => {
-  let audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
-  let tune = document.querySelector(`.tune[data-key='${event.keyCode}']`);
-  if(tune != null) {
+document.addEventListener("keydown", (e) => {
+  let audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  let tune = document.querySelector(`.tune[data-key='${e.keyCode}']`);
+  if (tune != null) {
     tune.classList.add("playing");
     audio.currentTime = 0;
     if (audio != null) audio.play();
-    
+
     setTimeout(function () {
       tune.classList.remove("playing");
     }, 100);
   }
-  });
+});
 
-function initRecording() {
-  isRecording = true;
-  tunes.splice(0, tunes.length);
-  timing.splice(0, timing.length)
+const records = [];
+const currentRecording = {
+  startTime: 0,
+  endTime: 0,
+  notes: [],
+};
+
+function recordingButtonPressed() {
+  changeRecordingStatus();
+  startRecording();
 }
 
-function stopRecording() {
-  isRecording = false;
+function startRecording() {
+  currentRecording.notes = [];
+  currentRecording.startTime = Date.now();
 }
 
-function recording(keyCode){
-  tunes.push(keyCode);
-  timing.push(Date.now());
-}
+function endRecording() {
+  currentRecording.endTime = Date.now();
+  console.log(currentRecording);
 
-function recordingControl(event){
-  if(event.keyCode == RECORDING_KEY){
-      if(!isRecording)
-        initRecording();
-      else
-        stopRecording();
+  if (currentRecording.notes.length > 0) {
+    const recordingWithTimeStamps = [];
+
+    for (let i = 0; i < currentRecording.notes.length; i++) {
+      recordingWithTimeStamps.push([
+        currentRecording.notes[i][0],
+        currentRecording.notes[i][1] - currentRecording.startTime,
+      ]);
+    }
+
+    records.push(recordingWithTimeStamps);
+    displayRecord();
   }
 }
 
-window.addEventListener("keydown", recordingControl)
+function recordSound(sound) {
+  currentRecording.notes.push([sound, Date.now()]);
+  // playSound(sound);
+}
+function onKeyPress(e) {
+  const sound = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (sound != null && isRecording == true) {
+    recordSound(sound);
+  }
+}
+
+function displayRecord() {
+  console.log(records);
+}
+
+function recordingControl(event) {
+  if (event.keyCode === Number(RECORDING_KEY)) {
+    if (!isRecording) {
+      console.log("Start");
+      isRecording = true;
+      startRecording();
+    } else {
+      isRecording = false;
+      console.log("Stop");
+      endRecording();
+    }
+  }
+}
+
+window.addEventListener("keydown", recordingControl);
